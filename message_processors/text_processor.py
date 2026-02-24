ec04fc99d8640fc633fc4ffc441264c8042ebe47
Mr. Python McPythony <PythonMcPythony@zerodaybootcamp.xyz>
2026-02-24 22:33:28 +0200

    some bizzar state, nothing works, commited before switching to master to verify it even works anymore

--- message_processors/text_processor.py
+++ message_processors/text_processor.py
@@ -3,6 +3,7 @@ from message_processors.base import BaseMessageProcessor
 class TextMessageProcessor(BaseMessageProcessor):
     async def process(self, message_doc: dict, target_instance) -> None:
         content = message_doc.get("content")
+        # Determine Recipient
         recipient_jid = target_instance.provider_instance.user_jid
         
         text_content = str(content)
